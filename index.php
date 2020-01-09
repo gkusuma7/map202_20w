13ba4aad2e13f0c184d1941dcd4c59d36be4f206
19051848 <gkusuma7@gmail.com>
2020-01-09 16:02:56 +0000

    lab1 commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<?php
+	
+$name ='Kusuma Garlapati';
+$date = date('Y-m-d H:i:s');
+$id = '19051848';
+$string1= 'Welcome to PHP LAB1';
+
+
+?>
+	
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <title>PHP LAB1 </title>
+</head>
+
+<body>
+	
+	<header>
+		
+		<h1>
+		 My Details
+	</h1>
+		
+	</header>
+	
+	<main>
+		
+		<section>
+		
+			<h2>
+				<?php echo $string1; ?>
+			</h2>
+			
+		<br>
+		
+
+			<h3>
+				My name is <?php echo $name; ?>
+		</h3>
+	
+	<br>
+	
+		<h4>
+			My student id is <?php echo $id; ?>
+			<br>
+			<br>
+			Today date is <?php echo $date; ?>
+	</h4>
+
+		
+		</section>
+		
+		
+		
+	</main>
+	
+	
+	</body>
+
+
+	
+</html>
