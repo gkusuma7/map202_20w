2032286f32f1fbe8fdc9f840e0564d35cb382ed3
19051848 <gkusuma7@gmail.com>
2020-01-16 17:03:17 +0000

    lab2 completed.

--- index.php
+++ index.php
@@ -2,70 +2,95 @@
 
 <?php
 	
-date_default_timezone_set('America/Toronto');
-$name ='Kusuma Garlapati';
-$date = date('Y-m-d H:i:s');
-$id = '19051848';
-$string1= 'Welcome to PHP LAB1';
+$films = array(
+            "comedy" => array (
+                            0 => "Pink Panther",
+                           1 => "johnny English",
+                            2 => "Tommy Boy"),
+            "action" => array (
+                            0 => "Die Hard",
+                            1 => "Expendables"),
+            "epic" => array (
+                            0 => "The Lord of the Rings"),
+            "Romance" => array (
+                            0 => "Romeo and Juliet")
+);
 
-
+$favorites = array(
+   array(
+       "name" => "Dave Punk",
+       "phone" => "5689741523",
+       "email" => "davepunk@gmail.com",
+   ),
+   array(
+       "name" => "Monty Smith",
+       "phone" => "2584369721",
+       "email" => "montysmith@gmail.com",
+   ),
+   array(
+       "name" => "John Flinch",
+       "phone" => "9875147536",
+       "email" => "johnflinch@gmail.com",)
+);
 ?>
-	
-<html lang="en">
-<head>
-   <meta charset="utf-8">
-   <title>PHP LAB1 </title>
-</head>
+    <html lang="en">
 
-<body>
-	
-	<header>
+    <head>
+        <meta charset="utf-8">
+        <title>PHP LAB2 </title>
+    </head>
+    <body>
+		<h1>
+			 Movies Array Table
+		</h1>
+        <table width:450px;margin:0; cellpadding="0" cellspacing="0"  border: 1px solid orange;>
+            <tr>
+                <th>Name</th>
+                <th>Genre</th>
+            </tr> 
+            <?php
+				foreach ($films as $genre => $film) {
+					foreach ($film as $movie) {
+						echo '<tr>';
+						echo '<td style="height:40px; width:200px; margin:0;">' . $movie . '</td>';
+						echo '<td style="height:40px; width:200px; margin:0;">' . $genre . '</td>';
+						echo '</tr>';
+					}
+
+				}
+			?>
+
+        </table>
 		
 		<h1>
-		 My Details
-	</h1>
+			Favourites Array Table
+		</h1>
 		
-	</header>
-	
-	<main>
 		
-		<section>
+		<table width:450px;margin:0; cellpadding="0" cellspacing="0" border="0">
+            <tr>
+                <th>Name</th>
+                <th>Phone</th>
+				<th>Email</th>
+            </tr> 
 		
-			<h2>
-				<?php echo $string1; ?>
-			</h2>
-			
-		<br>
+            <?php
+				foreach($favorites as $person)
+				{
+					echo'<tr>';
+					foreach($person as $details )
+					{
+						echo '<td style="height:40px; width:100px; margin:0;">' . $details . '</td>';
+					}
+					echo'</tr>';
+				}
+			?>
+
+        </table>
+		
+    </body>
 		
 
-			<h3>
-				My name is <?php echo $name; ?>
-		</h3>
-	
-	<br>
-	
-		<h4>
-			My student id is <?php echo $id; ?>
 		
-	</h4>
-			
-			<br>
-			
-			<p>
-					
-			<strong> Today date is <?php echo $date; ?> </strong>
-			</p>
 
-		
-		</section>
-		
-		
-		
-	</main>
-	
-	
-	</body>
-
-
-	
-</html>
+    </html>
